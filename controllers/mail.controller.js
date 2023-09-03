0f3346435d3c97b88f90a849b165ce4a19bb310a
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2023-09-03 23:11:33 +0200

    install nodemailer package, add postMail controller

--- controllers/mail.controller.js
+++ controllers/mail.controller.js
@@ -8,7 +8,10 @@ exports.getMail = async (req, res) => {
 
 exports.postMail = async (req, res) => {
   try {
-    res.json({ message: 'post works' });
+    const { name, email, subject, message } = req.body;
+    res.json({
+      message: `post works - ${[name, email, subject, message]} - ok`,
+    });
   } catch (err) {
     res.status(500).json({ message: err });
   }
