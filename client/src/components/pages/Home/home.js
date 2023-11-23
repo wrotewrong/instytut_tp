feed681c2ba8846a447d455134fc5bdb5796ba9c
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2023-11-23 18:18:54 +0100

    Add about section to the Home component

--- client/src/components/pages/Home/home.js
+++ client/src/components/pages/Home/home.js
@@ -90,6 +90,41 @@ export function Home() {
           </div>
         </div>
       </section>
+
+      <section className={styles.about}>
+        {/* <div className={styles.container}> */}
+        <div className={styles.aboutWrapper}>
+          <div className={styles.aboutPhoto}>
+            <img src='owner.webp' alt='the owner' />
+          </div>
+          <div className={styles.aboutDescription}>
+            <div className={styles.aboutHeader}>
+              Witam Państwa na stronie Instytutu Terapii
+            </div>
+            <div className={styles.aboutOwner}>
+              Jestem założycielem i właścicielem Poradni
+              Psychologiczno-Seksuologicznej
+            </div>
+            <div className={styles.aboutText}>
+              Jesteśmy specjalistycznym ośrodkiem zapewniającym kompleksową
+              pomoc psychologiczną i seksuologiczną dla dorosłych i dzieci.
+              Naszym celem jest szybka i efektywna pomoc osobom borykającym się
+              z problemami natury psychologicznej i seksualnej. Zespół naszych
+              specjalistów zapewnia profesjonalną opiekę w oparciu o najwyższe
+              standardy i najnowszą wiedzę. W naszym ośrodku uzyskają Państwo
+              pomoc, która pozwoli poradzić sobie z problemami oraz uczynić
+              Państwa życie takim jakiego Państwo chcą. Nasi specjaliści łączą
+              wieloletnie doświadczenie z wiedzą ekspercką i pasją, co pozwala
+              im pomagać pacjentom w realizacji ich celów.{' '}
+            </div>
+            <div className={styles.ownerSign}>
+              {' '}
+              Tomasz Poręba Psycholog, Seksuolog, Terapeuta{' '}
+            </div>
+          </div>
+        </div>
+        {/* </div> */}
+      </section>
     </div>
   );
 }
