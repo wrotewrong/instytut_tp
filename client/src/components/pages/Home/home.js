ddbbf9b3a8e486cbb5eb987024490e4b00ad952e
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2023-11-25 20:51:34 +0100

    Change styles of workAreas section of Home component

--- client/src/components/pages/Home/home.js
+++ client/src/components/pages/Home/home.js
@@ -15,77 +15,80 @@ export function Home() {
       </div>
 
       <section className={styles.workAreas}>
+        <h2>Obszary pracy:</h2>
         <div className={styles.container}>
-          <div className={styles.animationWrapper}>
-            <div className={styles.animation}>
-              <ScrollVideo url='puzzle_final.mp4'></ScrollVideo>
+          <div className={styles.workAreasWrapper}>
+            <div className={styles.animationWrapper}>
+              <div className={styles.animation}>
+                <ScrollVideo url='puzzle_final.mp4'></ScrollVideo>
+              </div>
+              <div className={styles.animationDescription}>
+                <p>Terapia indywidualna</p>
+                <p>Pomagamy lepiej radzić sobie w życiu</p>
+              </div>
             </div>
-            <div className={styles.animationDescription}>
-              <p>Terapia indywidualna</p>
-              <p>Pomagamy lepiej radzić sobie w życiu</p>
+            <div className={styles.animationWrapper}>
+              <div className={styles.animation}>
+                <ScrollVideo url='brain_final.mp4'></ScrollVideo>
+              </div>
+              <div className={styles.animationDescription}>
+                <p>Diagnostyka</p>
+                <p>Diagnoza osobowości oraz funkcji poznawczych</p>
+              </div>
             </div>
-          </div>
-          <div className={styles.animationWrapper}>
-            <div className={styles.animation}>
-              <ScrollVideo url='brain_final.mp4'></ScrollVideo>
+            <div className={styles.animationWrapper}>
+              <div className={styles.animation}>
+                <ScrollVideo url='her_final.mp4'></ScrollVideo>
+              </div>
+              <div className={styles.animationDescription}>
+                <p>Seksualność kobiet</p>
+                <p>Problemy w sferze seksualnej u kobiet </p>
+              </div>
             </div>
-            <div className={styles.animationDescription}>
-              <p>Diagnostyka</p>
-              <p>Diagnoza osobowości oraz funkcji poznawczych</p>
+            <div className={styles.animationWrapper}>
+              <div className={styles.animation}>
+                <ScrollVideo url='his_final.mp4'></ScrollVideo>
+              </div>
+              <div className={styles.animationDescription}>
+                <p>Seksualność mężczyzn</p>
+                <p>Problemy w sferze seksualnej u mężczyzn </p>
+              </div>
             </div>
-          </div>
-          <div className={styles.animationWrapper}>
-            <div className={styles.animation}>
-              <ScrollVideo url='her_final.mp4'></ScrollVideo>
+            <div className={styles.animationWrapper}>
+              <div className={styles.animation}>
+                <ScrollVideo url='relations with text.mp4'></ScrollVideo>
+              </div>
+              <div className={styles.animationDescription}>
+                <p>Relacje</p>
+                <p>Terapia par i małżeństw oraz problemy seksualne par </p>
+              </div>
             </div>
-            <div className={styles.animationDescription}>
-              <p>Seksualność kobiet</p>
-              <p>Problemy w sferze seksualnej u kobiet </p>
+            <div className={styles.animationWrapper}>
+              <div className={styles.animation}>
+                <ScrollVideo url='kid_final.mp4'></ScrollVideo>
+              </div>
+              <div className={styles.animationDescription}>
+                <p>Dzieci i młodzież</p>
+                <p>Terapia oraz diagnostyka dzieci i młodzieży</p>
+              </div>
             </div>
-          </div>
-          <div className={styles.animationWrapper}>
-            <div className={styles.animation}>
-              <ScrollVideo url='his_final.mp4'></ScrollVideo>
+            <div className={styles.animationWrapper}>
+              <div className={styles.animation}>
+                <ScrollVideo url='conflict_final.mp4'></ScrollVideo>
+              </div>
+              <div className={styles.animationDescription}>
+                <p>Konflikty</p>
+                <p>Pomagamy w rozwiązywaniu konfliktów</p>
+              </div>
             </div>
-            <div className={styles.animationDescription}>
-              <p>Seksualność mężczyzn</p>
-              <p>Problemy w sferze seksualnej u mężczyzn </p>
-            </div>
-          </div>
-          <div className={styles.animationWrapper}>
-            <div className={styles.animation}>
-              <ScrollVideo url='relations with text.mp4'></ScrollVideo>
-            </div>
-            <div className={styles.animationDescription}>
-              <p>Relacje</p>
-              <p>Terapia par i małżeństw oraz problemy seksualne par </p>
-            </div>
-          </div>
-          <div className={styles.animationWrapper}>
-            <div className={styles.animation}>
-              <ScrollVideo url='kid_final.mp4'></ScrollVideo>
-            </div>
-            <div className={styles.animationDescription}>
-              <p>Dzieci i młodzież</p>
-              <p>Terapia oraz diagnostyka dzieci i młodzieży</p>
-            </div>
-          </div>
-          <div className={styles.animationWrapper}>
-            <div className={styles.animation}>
-              <ScrollVideo url='conflict_final.mp4'></ScrollVideo>
-            </div>
-            <div className={styles.animationDescription}>
-              <p>Konflikty</p>
-              <p>Pomagamy w rozwiązywaniu konfliktów</p>
-            </div>
-          </div>
-          <div className={styles.animationWrapper}>
-            <div className={styles.animation}>
-              <ScrollVideo url='diet.mp4'></ScrollVideo>
-            </div>
-            <div className={styles.animationDescription}>
-              <p>Psychodietetyka</p>
-              <p>Pomoc w zaburzeniach związanych z obszarem odżywiania się</p>
+            <div className={styles.animationWrapper}>
+              <div className={styles.animation}>
+                <ScrollVideo url='diet.mp4'></ScrollVideo>
+              </div>
+              <div className={styles.animationDescription}>
+                <p>Psychodietetyka</p>
+                <p>Pomoc w zaburzeniach związanych z obszarem odżywiania się</p>
+              </div>
             </div>
           </div>
         </div>
