b7767cc589968b01318603f61bbe80ba80435f0f
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2023-12-06 21:05:22 +0100

    Add Footer component and its styles

--- client/src/components/layout/Footer/footer.js
+++ client/src/components/layout/Footer/footer.js
@@ -1,7 +1,14 @@
+import styles from './footer.module.scss';
+
 export function Footer() {
   return (
-    <div>
-      <p>Footer</p>
+    <div className={styles.footer}>
+      <div className={styles.footerWrapper}>
+        <h1 className={styles.footerLogo}>Instytut Terapii</h1>
+        <h2 className={styles.footerLogoText}>
+          Poradnia psychologiczno - seksuologiczna
+        </h2>
+      </div>
     </div>
   );
 }
