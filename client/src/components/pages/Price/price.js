95bbc0881ab4eb27b79cf363968d3f60f4e7cf52
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2023-12-19 23:31:54 +0100

    Add styles to Price component

--- client/src/components/pages/Price/price.js
+++ client/src/components/pages/Price/price.js
@@ -2,32 +2,30 @@ import styles from './price.module.scss';
 
 export function Price() {
   return (
-    <>
+    <div className={styles.priceComponent}>
       <div className='container'>
-        <div className={styles.priceWrapper}>
+        <div className={styles.servicesWrapper}>
           <h3>Ceny naszych usług:</h3>
-          <div className={styles.service}>
-            <p>Terapia indywidualna</p>
-            <p>140 zł</p>
-            <p>godzina</p>
-          </div>
-          <div className={styles.service}>
-            <p>Terapia par</p>
-            <p>160 zł</p>
-            <p>godzina</p>
-          </div>
-          <div className={styles.service}>
-            <p>Terapia seksuologiczna</p>
-            <p>150 zł</p>
-            <p>godzina</p>
-          </div>
-          <div className={styles.service}>
-            <p>Diagnostyka</p>
-            <p>150 zł</p>
-            <p>spotkanie</p>
+          <div className={styles.services}>
+            <div className={styles.service}>
+              <p>Terapia indywidualna</p>
+              <p>140 zł / godzina</p>
+            </div>
+            <div className={styles.service}>
+              <p>Terapia par</p>
+              <p>160 zł / godzina</p>
+            </div>
+            <div className={styles.service}>
+              <p>Terapia seksuologiczna</p>
+              <p>150 zł / godzina</p>
+            </div>
+            <div className={styles.service}>
+              <p>Diagnostyka</p>
+              <p>150 zł / spotkanie</p>
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
