c74dd44ae46153459cc9f88a8dabe7a8c48dd66c
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2023-12-27 23:53:18 +0100

    Add styles to MailForm component

--- client/src/components/features/MailForm/MailForm.js
+++ client/src/components/features/MailForm/MailForm.js
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { postMailRequest } from '../../../redux/mailRedux';
 import { config } from '../../../clientConfig';
+import styles from './mailForm.module.scss';
 
 export function MailForm() {
   const [name, setName] = useState('');
@@ -49,111 +50,130 @@ export function MailForm() {
   } = useForm();
 
   return (
-    <Form onSubmit={validate(handleSubmit)}>
-      <Form.Group controlId='formLogin'>
-        <Form.Label>Imię</Form.Label>
-        <Form.Control
-          {...register('name', {
-            required: true,
-            minLength: config.nameCharacterMin,
-            maxLength: config.nameCharacterMax,
-          })}
-          type='text'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        ></Form.Control>
-        {errors.name && (
-          <small>
-            Imię musi zawierać od {config.nameCharacterMin} do{' '}
-            {config.nameCharacterMax} znaków
-          </small>
-        )}
-      </Form.Group>
+    <div className={styles.messageComponent}>
+      <Form onSubmit={validate(handleSubmit)}>
+        <div className={styles.inputWrapper}>
+          <Form.Group controlId='formLogin'>
+            {/* <Form.Label>Imię</Form.Label> */}
+            <Form.Control
+              {...register('name', {
+                required: true,
+                minLength: config.nameCharacterMin,
+                maxLength: config.nameCharacterMax,
+              })}
+              type='text'
+              placeholder='Imię'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></Form.Control>
+            {errors.name && (
+              <small>
+                Imię musi zawierać od {config.nameCharacterMin} do{' '}
+                {config.nameCharacterMax} znaków
+              </small>
+            )}
+          </Form.Group>
+        </div>
 
-      <Form.Group controlId='formLogin'>
-        <Form.Label>Email</Form.Label>
-        <Form.Control
-          {...register('email', {
-            required: true,
-            maxLength: config.emailCharacterMax,
-            pattern: config.emailFormat,
-          })}
-          type='email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        ></Form.Control>
-        {errors.email && (
-          <small>
-            Email musi mieć odpowiedni format i mieć nie więcej niż{' '}
-            {config.emailCharacterMax} znaków
-          </small>
-        )}
-      </Form.Group>
+        <div className={styles.inputWrapper}>
+          <Form.Group controlId='formLogin'>
+            {/* <Form.Label>Email</Form.Label> */}
+            <Form.Control
+              {...register('email', {
+                required: true,
+                maxLength: config.emailCharacterMax,
+                pattern: config.emailFormat,
+              })}
+              type='email'
+              placeholder='Email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+            {errors.email && (
+              <small>
+                Email musi mieć odpowiedni format i mieć nie więcej niż{' '}
+                {config.emailCharacterMax} znaków
+              </small>
+            )}
+          </Form.Group>
+        </div>
 
-      <Form.Group controlId='formLogin'>
-        <Form.Label>Temat</Form.Label>
-        <Form.Control
-          {...register('subject', {
-            required: true,
-            minLength: config.subjectCharacterMin,
-            maxLength: config.subjectCharacterMax,
-          })}
-          type='text'
-          value={subject}
-          onChange={(e) => setSubject(e.target.value)}
-        ></Form.Control>
-        {errors.subject && (
-          <small>
-            Temat musi zawierać od {config.subjectCharacterMin} do{' '}
-            {config.subjectCharacterMax} znaków
-          </small>
-        )}
-      </Form.Group>
+        <div className={styles.inputWrapper}>
+          <Form.Group controlId='formLogin'>
+            {/* <Form.Label>Temat</Form.Label> */}
+            <Form.Control
+              {...register('subject', {
+                required: true,
+                minLength: config.subjectCharacterMin,
+                maxLength: config.subjectCharacterMax,
+              })}
+              type='text'
+              placeholder='Temat'
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
+            ></Form.Control>
+            {errors.subject && (
+              <small>
+                Temat musi zawierać od {config.subjectCharacterMin} do{' '}
+                {config.subjectCharacterMax} znaków
+              </small>
+            )}
+          </Form.Group>
+        </div>
 
-      <Form.Group controlId='formLogin'>
-        <Form.Label>Telefon</Form.Label>
-        <Form.Control
-          {...register('tel', {
-            required: true,
-            minLength: config.phoneCharacterMin,
-            maxLength: config.phoneCharacterMax,
-          })}
-          type='tel'
-          value={tel}
-          onChange={(e) => setTel(e.target.value)}
-        ></Form.Control>
-        {errors.tel && (
-          <small>
-            Numer telefonu musi zawierać od {config.phoneCharacterMin} do{' '}
-            {config.phoneCharacterMax} znaków
-          </small>
-        )}
-      </Form.Group>
+        <div className={styles.inputWrapper}>
+          <Form.Group controlId='formLogin'>
+            {/* <Form.Label>Telefon</Form.Label> */}
+            <Form.Control
+              {...register('tel', {
+                required: true,
+                minLength: config.phoneCharacterMin,
+                maxLength: config.phoneCharacterMax,
+              })}
+              type='tel'
+              placeholder='Telefon'
+              value={tel}
+              onChange={(e) => setTel(e.target.value)}
+            ></Form.Control>
+            {errors.tel && (
+              <small>
+                Numer telefonu musi zawierać od {config.phoneCharacterMin} do{' '}
+                {config.phoneCharacterMax} znaków
+              </small>
+            )}
+          </Form.Group>
+        </div>
 
-      <Form.Group controlId='formLogin'>
-        <Form.Label>Wiadomość</Form.Label>
-        <Form.Control
-          {...register('message', {
-            required: true,
-            minLength: config.messageCharacterMin,
-            maxLength: config.messageCharacterMax,
-            pattern: config.messageFormat,
-          })}
-          type='text'
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        ></Form.Control>
-        {errors.message && (
-          <small>
-            Wiadomość musi zawierać od {config.messageCharacterMin} do{' '}
-            {config.messageCharacterMax} liter lub cyfr
-          </small>
-        )}
-      </Form.Group>
+        <div className={styles.inputWrapper}>
+          <Form.Group controlId='formLogin'>
+            {/* <Form.Label>Wiadomość</Form.Label> */}
+            <Form.Control
+              as='textarea'
+              rows={5}
+              {...register('message', {
+                required: true,
+                minLength: config.messageCharacterMin,
+                maxLength: config.messageCharacterMax,
+                pattern: config.messageFormat,
+              })}
+              type='text'
+              placeholder='Wiadomość'
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></Form.Control>
+            {errors.message && (
+              <small>
+                Wiadomość musi zawierać od {config.messageCharacterMin} do{' '}
+                {config.messageCharacterMax} liter lub cyfr
+              </small>
+            )}
+          </Form.Group>
+        </div>
 
-      <Button type='submit' className='mt-3'>
-        Wyślij
-      </Button>
-    </Form>
+        <Button type='submit' className='mt-3'>
+          Wyślij
+        </Button>
+      </Form>
+    </div>
   );
 }
--- client/src/components/pages/Contact/contact.js
+++ client/src/components/pages/Contact/contact.js
@@ -1,10 +1,15 @@
 import { MailForm } from '../../features/MailForm/MailForm';
+import styles from './contact.module.scss';
 
 export function Contact() {
   return (
     <>
-      <div>Contact</div>
-      <MailForm></MailForm>
+      <div className='container'>
+        <div className={styles.contactWrapper}>
+          <h3>Wyślij wiadomość:</h3>
+          <MailForm></MailForm>
+        </div>
+      </div>
     </>
   );
 }
